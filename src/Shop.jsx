727d642c81309028ffe52445c8e12a2999f1cfca
Herbert Gutierrez <dev.gutti1391@gmail.com>
2024-01-01 17:10:11 -0600

    First commit

---
+++ src/Shop.jsx
@@ -0,0 +1,11 @@
+export const Shop = () => {
+  return (
+    <>
+      <div>Shop</div>
+
+      <button type="button" className="btn btn-primary">
+        Primary
+      </button>
+    </>
+  );
+};
