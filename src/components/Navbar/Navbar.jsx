9f00b595a9cd008cb76f7afc3c1409ff50e1dafa
Herbert Gutierrez <dev.gutti1391@gmail.com>
2024-01-06 10:22:14 -0600

    Navbar component 80% completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,59 @@
+import { useContext, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../assets/logo.png";
+import nav_dropdown from "../assets/nav_dropdown.png";
+import cart_icon from "../assets/cart_icon.png";
+
+import "./Navbar.css";
+
 export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
+  console.log(menu);
+
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-      <div className="container-fluid">
-        <Link className="navbar-brand">Movies App</Link>
-        <form className="d-flex" role="search">
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
+    <nav className="navbar">
+      <div className="nav-logo">
+        <img src={logo} />
+        <p>SHOPPER</p>
+      </div>
+      {/* <img src={nav_dropdown} alt="" /> */}
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop
+          {menu === "shop" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          Men {menu === "mens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          Women {menu === "womens" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids {menu === "kids" ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count"></div>
       </div>
     </nav>
   );
